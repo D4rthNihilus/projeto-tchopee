9a061535ef43cdc41680dab4e6110fc4f2f07261
D4rthNihilus <rafaelguedes05013@gmail.com>
2025-04-20 21:24:05 -0300

    Agora é possível enviar seu comentário!

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-function estciv() {
-    estciv = document.getElementsByName('matstatus')
-    qjp = document.querySelector('div#qjp')
+let c1 = document.getElementById('com1')
+let hr1 = document.getElementById('uhr1')
+let c2 = document.getElementById('com2')
+let hr2 = document.getElementById('uhr2')
+let c3 = document.getElementById('com3')
+let hr3 = document.getElementById('uhr3')
+let next = 3
+let txt = document.getElementById('text')
+let c1txt = document.getElementById('c1txt')
+let c2txt = document.getElementById('c2txt')
+let c3txt = document.getElementById('c3txt')
 
-    if(estciv[2].checked) {
-        qjp.style.display ='block'
+function comment() {
+
+    if (next == 3) {
+        c3.style.display = 'block'
+        hr3.style.display = 'block'
+
+        c3txt.innerHTML = txt.value
+        next = 2
+    } else {
+        if (next == 2) {
+            c2.style.display = 'block'
+            hr2.style.display = 'block'
+
+            c2txt.innerHTML = txt.value
+            next = 1
+        } else {
+            if (next == 1) {
+                c1.style.display = 'block'
+                hr1.style.display = 'block'
+
+                c1txt.innerHTML = txt.value
+                next = 0
+            } else {
+                window.alert('Você só pode enviar até 3 comentários')
+            }
+        }
     }
-}
+
+    txt.value = ''
+} 
